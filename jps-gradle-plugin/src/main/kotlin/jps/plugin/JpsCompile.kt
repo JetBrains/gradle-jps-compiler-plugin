e425970cd1a75a91487cd9c3ec32f1cacea2619c
Titouan Bion <titouan.bion@jetbrains.com>
2023-07-15 16:58:22 +0200

    Make `JpsCompile` and `JpsPlugin` compliant with configuration cache
    Includes
    - removal of `project` usage at execution phase
    - Dependencies (JPS wrapper, JPS standalone, kotlin dist and kotlin jps plugin) are now wired using configuration that respects configuration cache requirements (we do not modify project at execution phase anymore)
    - It is still possible to specify different versions in task properties, the plugin will wire the necessary configurations to accommodate the run
    - Dependencies will still be downloaded lazily (right before execution, instead of during start of execution phase)
    - JpsCompile is now using `Provider`/`Property` lazy interfaces to be a better (lazy) citizen in user's builds
    - `kotlinVersion` is now mandatory to specify (it was not working without it during my testing)

--- jps-gradle-plugin/src/main/kotlin/jps/plugin/JpsCompile.kt
+++ jps-gradle-plugin/src/main/kotlin/jps/plugin/JpsCompile.kt
@@ -1,110 +1,153 @@
 package jps.plugin
 
 import org.gradle.api.DefaultTask
+import org.gradle.api.file.ArchiveOperations
+import org.gradle.api.file.FileSystemOperations
+import org.gradle.api.file.ProjectLayout
+import org.gradle.api.model.ObjectFactory
+import org.gradle.api.provider.ProviderFactory
 import org.gradle.api.tasks.*
-import org.gradle.kotlin.dsl.findByType
+import org.gradle.kotlin.dsl.listProperty
+import org.gradle.kotlin.dsl.mapProperty
+import org.gradle.kotlin.dsl.property
+import org.gradle.process.ExecOperations
 import java.io.File
 import java.nio.file.Files
+import javax.inject.Inject
 
 @Suppress("unused")
-open class JpsCompile : DefaultTask() {
+abstract class JpsCompile @Inject constructor(
+    objectFactory: ObjectFactory,
+    providerFactory: ProviderFactory,
+    private val archiveOperations: ArchiveOperations,
+    private val execOperations: ExecOperations,
+    private val fs: FileSystemOperations,
+    private val projectLayout: ProjectLayout,
+) : DefaultTask() {
     companion object {
         const val PROPERTY_PREFIX = "build"
-        const val DEFAULT_JPS_WRAPPER_VERSION = "0.24"
-        const val DEFAULT_JPS_VERSION = "2022.1"
     }
+
+    @Input
+    val jpsVersion = objectFactory.property<String>()
+
+    @Input
+    val jpsWrapperVersion = objectFactory.property<String>()
+
+    @Input
+    @Optional
+    val moduleName = objectFactory.property<String>()
+
+    @Input
+    @Optional
+    val projectPath = objectFactory.property<String>()
+
+    @OutputFile
+    val classpathOutputFilePath = objectFactory.fileProperty()
+
+    @Input
+    val incremental = objectFactory.property<Boolean>()
+
+    @Input
+    val includeRuntimeDependencies = objectFactory.property<Boolean>()
+
+    @Input
+    val includeTests = objectFactory.property<Boolean>()
+
+    @Input
+    val parallel = objectFactory.property<Boolean>()
+
+    @Input
+    val withProgress = objectFactory.property<Boolean>()
+
+    @Input
+    val kotlinVersion = objectFactory.property<String>()
+
+    @InputFile
+    val kotlinDistZip = objectFactory.fileProperty()
+
+    @InputFile
+    val kotlinJpsPlugin = objectFactory.fileProperty()
+
+    @InputFile
+    val jpsWrapper = objectFactory.fileProperty()
+
+    @InputFile
+    val jpsStandaloneZip = objectFactory.fileProperty()
+
+    @Optional
+    @Input
+    val systemProperties = objectFactory.mapProperty<String, String>()
+
+    @Optional
+    @Input
+    val jvmArgs = objectFactory.listProperty<String>()
+
+    @OutputDirectory
+    val outputPath = objectFactory.directoryProperty()
+
+    @Input
+    @Optional
+    val jdkTableContent = objectFactory.mapProperty<String, String>()
 
     init {
         outputs.upToDateWhen { false }
+        jpsVersion.convention(DEFAULT_JPS_VERSION)
+        jpsWrapperVersion.convention(DEFAULT_JPS_WRAPPER_VERSION)
+        classpathOutputFilePath.convention(projectLayout.file(providerFactory.provider {
+            Files.createTempFile(temporaryDir.toPath(), "classpath", "").toFile()
+        }))
+        incremental.convention(true)
+        includeRuntimeDependencies.convention(true)
+        includeTests.convention(true)
+        parallel.convention(true)
+        withProgress.convention(false)
+        outputPath.convention(projectLayout.buildDirectory.dir("jps/out"))
     }
-
-    @Input
-    var jpsVersion: String = DEFAULT_JPS_VERSION
-
-    @Input
-    var jpsWrapperVersion: String = DEFAULT_JPS_WRAPPER_VERSION
-
-    @Input
-    var moduleName: String? = null
-
-    @Input
-    var projectPath: String? = null
-
-    @OutputFile
-    var classpathOutputFilePath: String = Files.createTempFile("classpath", "").toString()
-
-    @Input
-    var incremental: Boolean = true
-
-    @Input
-    var includeRuntimeDependencies: Boolean = true
-
-    @Input
-    var includeTests: Boolean = true
-
-    @Input
-    var parallel: Boolean = true
-
-    @Input
-    var withProgress: Boolean = false
-
-    @Optional
-    @Input
-    var kotlinVersion: String? = null
-
-    @Optional
-    @InputFile
-    var jpsWrapper: File? = null
-
-    @Optional
-    @Input
-    var systemProperties: Map<String, String> = emptyMap()
-
-    @Optional
-    @Input
-    var jvmArgs: List<String> = emptyList()
-
-    @Input
-    var outputPath: String = "${project.buildDir}/jps/out"
-
-    private val jdkTable = File(project.buildDir, "jdkTable.txt")
 
     @TaskAction
     fun compile() {
-        val jpsWrapper = jpsWrapper ?: project.downloadJpsWrapper(jpsWrapperVersion)
-        val jpsStandaloneDirectory = project.downloadJpsStandalone(jpsVersion)
-        val jpsClasspath = project.file(jpsStandaloneDirectory).listFiles()?.toList() ?: emptyList()
-        val kotlinDirectory = kotlinVersion?.let { pluginVersion ->
-            project.downloadKotlin(pluginVersion)
-        }
-        val kotlinJpsPlugin = kotlinVersion?.let { pluginVersion ->
-            project.downloadKotlinJpsPlugin(pluginVersion)
-        }
+        val kotlinDirectory = extractZip(kotlinDistZip.get().asFile, destinationFolderName = "kotlin-dist")
+        val jpsStandaloneDirectory = extractZip(jpsStandaloneZip.get().asFile, destinationFolderName = "jps-standalone")
+        val jpsClasspath = jpsStandaloneDirectory.listFiles()?.toList() ?: emptyList()
 
-        val jdkTableContent = project.extensions.findByType(JdkTableExtension::class)?.jdkTable ?: emptyMap()
-        project.buildDir.mkdirs()
-        jdkTable.writeText(jdkTableContent.map { (k, v) -> "$k=$v" }.joinToString("\n"))
+        val jdkTable = temporaryDir.resolve("jdkTable.txt")
+        jdkTable.writeText(jdkTableContent.get().entries.joinToString("\n") { (k, v) -> "$k=$v" })
 
-        val extraProperties = systemProperties
-        val extraJvmArgs = jvmArgs
-        project.javaexec {
-            classpath = project.files(jpsWrapper, jpsClasspath, kotlinJpsPlugin)
+        val extraProperties = systemProperties.orNull
+        val extraJvmArgs = jvmArgs.orNull
+        execOperations.javaexec {
+            classpath = projectLayout.files(jpsWrapper.asFile, jpsClasspath, kotlinJpsPlugin.asFile)
             mainClass.set("jps.wrapper.MainKt")
 
             listOf(
-                JpsCompile::moduleName, JpsCompile::projectPath, JpsCompile::classpathOutputFilePath,
-                JpsCompile::includeTests, JpsCompile::includeRuntimeDependencies, JpsCompile::incremental,
-                JpsCompile::parallel, JpsCompile::withProgress, JpsCompile::jdkTable, JpsCompile::outputPath
-            ).forEach { property ->
-                systemProperty(property.name.withPrefix(), property.get(this@JpsCompile)?.toString())
+                "moduleName" to moduleName.orNull,
+                "projectPath" to projectPath.orNull,
+                "classpathOutputFilePath" to classpathOutputFilePath.get().asFile.absolutePath,
+                "includeTests" to includeTests.get().toString(),
+                "includeRuntimeDependencies" to includeRuntimeDependencies.get().toString(),
+                "incremental" to incremental.get().toString(),
+                "parallel" to parallel.get().toString(),
+                "withProgress" to withProgress.get().toString(),
+                "jdkTable" to jdkTable.absolutePath,
+                "outputPath" to outputPath.get().asFile.absolutePath,
+            ).forEach { (name, value) ->
+                systemProperty(name.withPrefix(), value)
             }
 
             systemProperties(extraProperties)
-            kotlinDirectory?.let {
-                systemProperty("kotlinHome".withPrefix(), kotlinDirectory)
-            }
+            systemProperty("kotlinHome".withPrefix(), kotlinDirectory)
             jvmArgs(extraJvmArgs)
         }
+    }
+
+    private fun extractZip(archive: File, destinationFolderName: String): File {
+        val dir = temporaryDir.resolve(destinationFolderName)
+        fs.sync {
+            from(archiveOperations.zipTree(archive))
+            into(dir)
+        }
+        return dir
     }
 
     private fun String.withPrefix() = "$PROPERTY_PREFIX.$this"
--- jps-gradle-plugin/src/main/kotlin/jps/plugin/JpsPlugin.kt
+++ jps-gradle-plugin/src/main/kotlin/jps/plugin/JpsPlugin.kt
@@ -1,14 +1,81 @@
 package jps.plugin
 
+import org.gradle.api.NamedDomainObjectProvider
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.UnknownDomainObjectException
+import org.gradle.api.artifacts.Configuration
+import org.gradle.api.provider.Provider
+import org.gradle.api.provider.SetProperty
 import org.gradle.kotlin.dsl.create
+import org.gradle.kotlin.dsl.maven
+import org.gradle.kotlin.dsl.repositories
+import org.gradle.kotlin.dsl.withType
+import java.io.File
+import java.util.regex.Pattern
+import kotlin.collections.set
+
+const val DEFAULT_JPS_WRAPPER_VERSION = "0.24"
+const val DEFAULT_JPS_VERSION = "2022.1"
 
 @Suppress("unused")
 class JpsPlugin : Plugin<Project> {
     override fun apply(project: Project) {
-        project.extensions.create(JdkTableExtension.EXTENSION_NAME, JdkTableExtension::class)
+        val jdkTableExtension = project.extensions.create<JdkTableExtension>(JdkTableExtension.EXTENSION_NAME)
+        val jpsVersionConfiguration = project.extensions.create<JpsVersionConfiguration>("jpsConfiguration")
+
+        jpsVersionConfiguration.jpsWrapperVersions.add(DEFAULT_JPS_WRAPPER_VERSION)
+        jpsVersionConfiguration.jpsVersions.add(DEFAULT_JPS_VERSION)
+
+        val jpsWrapperConfigurations = project.registerConfigurations(
+            jpsVersionConfiguration.jpsWrapperVersions,
+            "jpsWrapper",
+            { "https://cache-redirector.jetbrains.com/intellij-dependencies" },
+            { "com.jetbrains.intellij.idea:jps-wrapper:$it" },
+        )
+        val jpsStandaloneConfigurations = project.registerConfigurations(
+            jpsVersionConfiguration.jpsVersions,
+            "jpsStandalone",
+            { "https://cache-redirector.jetbrains.com/www.jetbrains.com/intellij-repository/${intellijRepositoryType(it)}" },
+            { "com.jetbrains.intellij.idea:jps-standalone:$it@zip" },
+        )
+        val kotlinDistConfigurations = project.registerConfigurations(
+            jpsVersionConfiguration.kotlinVersions,
+            "kotlinDist",
+            { "https://cache-redirector.jetbrains.com/maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-ide-plugin-dependencies" },
+            { "org.jetbrains.kotlin:kotlin-dist-for-ide:$it@jar" },
+        )
+        val kotlinJpsPluginConfigurations = project.registerConfigurations(
+            jpsVersionConfiguration.kotlinVersions,
+            "kotlinJpsPlugin",
+            { "https://cache-redirector.jetbrains.com/maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-ide-plugin-dependencies" },
+            { "org.jetbrains.kotlin:kotlin-jps-plugin-classpath:$it@jar" },
+        )
+
+        project.tasks.withType<JpsCompile> {
+            // for each declaration of JpsCompile, we record the version the user specified so that we can be prepared
+            // to download it when it will be required
+            jpsVersionConfiguration.jpsWrapperVersions.add(jpsWrapperVersion)
+            jpsVersionConfiguration.jpsVersions.add(jpsVersion)
+            jpsVersionConfiguration.kotlinVersions.add(kotlinVersion)
+
+            jdkTableContent.set(project.provider { jdkTableExtension.jdkTable })
+
+            // all these wiring are lazy, configurations will not be resolved, dependencies not downloaded until
+            // the JpsCompile task is about to be executed.
+            // This is the laziest we can be while being compliant to the Gradle configuration cache.
+            jpsWrapper.set(project.layout.file(jpsWrapperConfigurations.dependencyFile(jpsWrapperVersion)))
+            jpsStandaloneZip.set(project.layout.file(jpsStandaloneConfigurations.dependencyFile(jpsVersion)))
+            kotlinDistZip.set(project.layout.file(kotlinDistConfigurations.dependencyFile(kotlinVersion)))
+            kotlinJpsPlugin.set(project.layout.file(kotlinJpsPluginConfigurations.dependencyFile(kotlinVersion)))
+        }
     }
+}
+
+interface JpsVersionConfiguration {
+    val kotlinVersions: SetProperty<String>
+    val jpsWrapperVersions: SetProperty<String>
+    val jpsVersions: SetProperty<String>
 }
 
 open class JdkTableExtension {
@@ -23,3 +90,67 @@ open class JdkTableExtension {
         jdkTable[name] = path
     }
 }
+
+private val MAJOR_VERSION_PATTERN = Pattern.compile("\\d{4}\\.\\d-SNAPSHOT")
+private fun intellijRepositoryType(version: String): String {
+    return when {
+        version.endsWith("-EAP-SNAPSHOT") || version.endsWith("-EAP-CANDIDATE-SNAPSHOT") || version.endsWith("-CUSTOM-SNAPSHOT") || MAJOR_VERSION_PATTERN.matcher(
+            version
+        ).matches() -> "snapshots"
+
+        version.endsWith("-SNAPSHOT") -> "nightly"
+        else -> "releases"
+    }
+}
+
+/**
+ * Lazily register one configuration per version of [versions] that contains the dependency [dependencyNotation] in
+ * repository [repository] for that version.
+ *
+ * This maintains compatibility with Gradle configuration cache.
+ *
+ * This is meant to prevent downloading using `project` and detached configurations at execution phase, while still
+ * being maintaining laziness.
+ *
+ * Meaning that configuration of a specific version will not be resolved (dependency not downloaded) if no [JpsCompile]
+ * task is requesting that version, and they will not be resolved if a [JpsCompile] task is specifying the version but
+ * not part of the task execution graph for the given run.
+ */
+private fun Project.registerConfigurations(
+    versions: Provider<Set<String>>,
+    name: String,
+    repository: (version: String) -> String,
+    dependencyNotation: (version: String) -> String
+): Provider<Map<String, NamedDomainObjectProvider<Configuration>>> = versions.map {
+    it.associateWith { version ->
+        val fullName = "${name}_$version"
+        try {
+            configurations.named(fullName)
+        } catch (e: UnknownDomainObjectException) {
+            configurations.register(fullName) {
+                repositories { maven(repository.invoke(version)) }
+                dependencies.add(project.dependencies.create(dependencyNotation.invoke(version)))
+            }
+        }
+    }
+}
+
+/**
+ * Returns the dependency file provider of the specified [version].
+ *
+ * Configuration will be resolved (and thus dependency downloaded) only when resulting [Provider]'s value is asked.
+ */
+private fun Provider<Map<String, NamedDomainObjectProvider<Configuration>>>.dependencyFile(
+    version: Provider<String>,
+): Provider<File> = map { configByVersion ->
+    val v = version.get()
+    val selected = configByVersion[v]?.get()
+        ?: unexpectedError("could not select version $v, available versions ${configByVersion.keys}")
+    val resolved = selected.resolve()
+    resolved.singleOrNull()
+        ?: unexpectedError("configuration '${selected.name}' must have only one file, found $resolved, this is unexpected contact plugin maintainers")
+}
+
+fun unexpectedError(message: Any): Nothing = error(
+    "$message\n\nThis is unexpected, please file an issue at https://github.com/JetBrains/gradle-jps-compiler-plugin/issues"
+)
