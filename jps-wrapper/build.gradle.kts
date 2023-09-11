cf172f78c7dfd6c63734305718235397085e759e
Titouan Bion <titouan.bion@jetbrains.com>
2023-09-11 14:53:34 +0200

    Bump `jps-wrapper` version to `0.31`

--- jps-wrapper/build.gradle.kts
+++ jps-wrapper/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 val jpsVersion = "221.3598"
 
-project.version = "0.30"
+project.version = "0.31"
 
 dependencies {
     compileOnly("com.jetbrains.intellij.tools:jps-build-standalone:$jpsVersion") {
